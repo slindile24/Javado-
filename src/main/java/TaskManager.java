ca3c1859c7d1795e6e697074ca82edba7f265da6
Slindile <slnkabijhb024@student.wethinkcode.co.za>
2025-03-08 22:08:58 +0200

    Task Manger

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,12 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.FileWriter;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
+    private static final String TEST_FILE= "tasks.csv";
+    private TaskManager taskManager;
+
+
+
 
     public TaskManager() {
         // Initialize tasks list
          tasks = new ArrayList<>();
+
+         try{
+             File file = new File(TEST_FILE);
+             if(file.createNewFile()){
+                 System.out.println(file);
+             }else{
+                 Files.deleteIfExists(Paths.get(TEST_FILE));
+                 taskManager = new TaskManager();
+             }
+         }catch(IOException e){
+             System.out.println("Error occured");
+             e.printStackTrace();
+         }
 
     }
 
@@ -16,7 +40,6 @@ public class TaskManager {
 
     public List<String> listTasks() {
         return tasks;
-
     }
 
     public void deleteTask(String task){
@@ -24,7 +47,14 @@ public class TaskManager {
     }
 
     public void exit() {
+        try{
+            Files.write(Paths.get(TEST_FILE), tasks);
+        }catch(IOException e){
+            System.out.println("Error occured");
+            e.printStackTrace();
+        }
         // leave for iteration 2
+//        File.write(Paths.get(TEST_FILE),tasks);
         
     }
 }
